8271271b722eabf0dc99134e785e5a270c29bd0a
universmc <universmc@hubmaster74.gmail.com>
2024-04-03 02:03:42 +0200

    SessionStart

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php 
+include 'inc/header.php';
+include 'inc/main.php';
+include 'inc/footer.php';
+?>
---
+++ output/js_20240402233745.120.js
@@ -0,0 +1,9 @@
+Merci! Voici le code JavaScript demandé pour afficher un message dans la console lors du chargement de la page d'index :
+
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+console.log('Page chargée avec succès !');
+});
+</script>
+
+En plaçant ce code dans un fichier script.js et en l'incluant dans votre page HTML, vous verrez apparaître le message "Page chargée avec succès !" dans la console JavaScript de votre navigateur une fois la page entièrement chargée.
